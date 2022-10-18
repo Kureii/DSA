dfbe4a86a9150ac14264aa9af20f29e594ebc5e7
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-18 09:12:01 +0000

    Adding tarfile member sanitization to extractall()

--- Fce.py
+++ Fce.py
@@ -365,7 +365,29 @@ def Verify(file, pub, sign):
     elif IsTar(file):
         wasArchive = True
         with tarfile.open(file, "r") as tar:
-            tar.extractall(newDir)
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, newDir)
     if wasArchive and sign == "":
         for i in listdir(newDir):
             if ".sign" in i:
